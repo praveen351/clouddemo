560bef8c1b67e19046bcd65ff86183ed5ffc586a
praveen351 <61460951+praveen351@users.noreply.github.com>
2020-05-01 20:15:50 +0530

    Change into a Sum file
    a =2
    b =3
    c = a + b

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,13 @@
+import java.util.*;
 public class HelloWorld{
   public static void main(String[] args){
-    System.out.println("Hello World!!!!!!!!!!!!!!");
+    Scanner sc = new Scanner(System.in);
+    int x , y , z;
+    System.out.println("Enter first number");
+    x = sc.nextInt();
+    System.out.println("Enter Second number");
+    y = sc.nextInt();
+    z = x + y;
+    System.out.println("The sum is :" + z);
   }
 }
